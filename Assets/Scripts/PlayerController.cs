331bac3314153e1385b4d9211ec7f9f4dd67eb2c
Livecan <roman.meszaros@gmail.com>
2021-09-23 07:46:36 +0200

    Player aligned by walls, not the global space.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,37 +9,20 @@ public class PlayerController : MonoBehaviour
     enum MovementState { Left, Right, Back, Forward, Stop };
 
     private MovementState? nextTurn;
-
-    private Rigidbody playerRigidbody;
     private float turningSpeed = 2;
     private float movingSpeed = 3;
+    private float tileSize = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRigidbody = GetComponent<Rigidbody>();
-    }
 
-    private void StartTurning(float degreesLeft, MovementState direction)
-    {
-        turnAngleLeft = degreesLeft;
-        playerMovementState = direction;
-        nextTurn = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Turn Left"))
-        {
-            nextTurn = MovementState.Left;
-        } else if (Input.GetButtonDown("Turn Right"))
-        {
-            nextTurn = MovementState.Right;
-        } else if (Input.GetButtonDown("Turn Back"))
-        {
-            nextTurn = MovementState.Back;
-        }
+        HandleUserInput();
 
         if ((playerMovementState == MovementState.Forward || playerMovementState == MovementState.Stop) && nextTurn == MovementState.Back)
         {
@@ -53,49 +36,90 @@ public class PlayerController : MonoBehaviour
 
         if (playerMovementState == MovementState.Left || playerMovementState == MovementState.Right || playerMovementState == MovementState.Back)
         {
-            float currentRotation = turningSpeed * 90 * Time.deltaTime;
-            transform.Rotate(Vector3.up, (playerMovementState == MovementState.Left ? -currentRotation : currentRotation));
-            turnAngleLeft -= currentRotation;
-            if (turnAngleLeft < 0)
-            {
-                transform.eulerAngles = new Vector3(
-                    transform.eulerAngles.x,
-                    Mathf.RoundToInt(transform.eulerAngles.y / 90) * 90,
-                    transform.eulerAngles.z);
-                playerMovementState = MovementState.Forward;
-            }
+            RotatePlayer();
         } else if (playerMovementState == MovementState.Forward)
         {
             transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
         }
     }
 
-    private void AlignPlayer()
+    // Sets player's state to turning in the given direction
+    private void StartTurning(float degreesLeft, MovementState direction)
     {
-        if (Mathf.Abs(transform.eulerAngles.y % 180) < 1)
+        turnAngleLeft = degreesLeft;
+        playerMovementState = direction;
+        nextTurn = null;
+    }
+
+    // Rotates the player according to the player's turning direction and rotation left
+    private void RotatePlayer()
+    {
+        float currentRotation = turningSpeed * 90 * Time.deltaTime;
+        transform.Rotate(Vector3.up, (playerMovementState == MovementState.Left ? -currentRotation : currentRotation));
+        turnAngleLeft -= currentRotation;
+        if (turnAngleLeft < 0)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Round(transform.position.z));
-        }
-        else
-        {
-            transform.position = new Vector3(Mathf.Round(transform.position.x), transform.position.y, transform.position.z);
+            transform.eulerAngles = new Vector3(
+                transform.eulerAngles.x,
+                Mathf.RoundToInt(transform.eulerAngles.y / 90) * 90,
+                transform.eulerAngles.z);
+            playerMovementState = MovementState.Forward;
         }
     }
 
+    // Registers user input - at the next turning point (when colliding with Wall/Turning Wall) this turning would be initiated.
+    private void HandleUserInput()
+    {
+        if (Input.GetButtonDown("Turn Left"))
+        {
+            nextTurn = MovementState.Left;
+        }
+        else if (Input.GetButtonDown("Turn Right"))
+        {
+            nextTurn = MovementState.Right;
+        }
+        else if (Input.GetButtonDown("Turn Back"))
+        {
+            nextTurn = MovementState.Back;
+        }
+    }
+
+    // Aligns player in appropriate distance from the given wall
+    private void AlignPlayer(GameObject wall)
+    {
+        if (Mathf.Abs(transform.eulerAngles.y % 180) < 1)
+        {
+            float zDimension = transform.position.z < wall.transform.position.z ?
+                wall.transform.position.z - tileSize / 2 :
+                wall.transform.position.z + tileSize / 2;
+            transform.position = new Vector3(transform.position.x, transform.position.y, zDimension);
+        }
+        else
+        {
+            float xDimension = transform.position.x < wall.transform.position.x ?
+                wall.transform.position.x - tileSize / 2 :
+                wall.transform.position.x + tileSize / 2;
+            transform.position = new Vector3(xDimension, transform.position.y, transform.position.z);
+        }
+    }
+
+    // After player hits a Wall, he stops and is put in appropriate distance from the wall.
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Wall"))
         {
             playerMovementState = MovementState.Stop;
-            AlignPlayer();
+            AlignPlayer(other.gameObject);
         }
     }
 
+    // After player hits hits a turning wall, if next turn is registered,
+    // it is started and the player is aligned in appropriate dostance from the wall.
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Turning Wall") && nextTurn != null)
         {
-            AlignPlayer();
+            AlignPlayer(other.gameObject);
             StartTurning(90, (MovementState)nextTurn);
         }
     }
